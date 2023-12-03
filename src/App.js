efdce48c9028c15a75aca28034e698059f71e176
Azeem Shaik <azeemshaik025@gmail.com>
2023-12-03 10:27:28 +0530

    Create Clinet Side Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Home from "../src/Components/Home/Home";
+import { Route, Routes } from 'react-router-dom';
+import Search from './Components/Search/Search';
+import Explore from './Components/Explore/Explore';
+import Message from './Components/Message/Message';
+import Profile from './Components/Profile/Profile';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path='/' exact Component={Home} />
+        <Route path='/search' exact Component={Search} />
+        <Route path='/explore' exact Component={Explore} />
+        <Route path='/message' exact Component={Message} />
+        <Route path='/profile' exact Component={Profile} />
+      </Routes>
     </div>
   );
 }
