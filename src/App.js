f21ad64076a759b23bfc0ad5b81de7aa18da0fae
Azeem Shaik <azeemshaik025@gmail.com>
2023-12-03 10:15:53 +0530

    Fixed Warning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
 import Home from "../src/Components/Home/Home";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Search from './Components/Search/Search';
-import Explore from './Components/Explore/Explore';
-import Message from './Components/Message/Message';
 function App() {
   return (
     <div className="App">
-      
-      <Routes>
-        <Route path='/' exact Component={Home} />
-        <Route path='/search' exact Component={Search} />
-        <Route path='/explore' exact Component={Explore} />
-        <Route path='/message' exact Component={Message} />
-      </Routes>
+      <Home />
     </div>
   );
 }
