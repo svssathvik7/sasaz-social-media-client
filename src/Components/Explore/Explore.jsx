d85bf9ca4789e35e873baf5426c8f4111e49f653
SATHVIK SVS <svssathvik77@gmail.com>
2023-12-05 11:44:45 +0530

    explore page finished with minor size styling problem as like home

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Explore.css';
+import exploreConstants from '../../Constants/exploreConstants';
 
 const Explore = () => {
-    return (
-        <div>Explore</div>
-    )
-}
+  return (
+    <div id='explore-page'>
+      {exploreConstants.map((section) => (
+        <div id='explore-section' key={section.tag}>
+          <h3 className='explore-section-tag'>#{section.tag}</h3>
+          <div id='explore-posts' className='explore-posts-container'>
+            {section.posts.map((post) => (
+              <div key={post.id} className='explore-feed'>
+                {post.type === 'image' ? (
+                  <div className='explore-post-img'>
+                    <img alt='post-img' src={post.post} />
+                  </div>
+                ) : (
+                  <div className='explore-post-post'>
+                    <h6>{post.post}</h6>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Explore
+export default Explore;
---
+++ src/Constants/exploreConstants.js
@@ -0,0 +1,16 @@
+import postConstants from "./PostConstants"
+const exploreConstants = [
+    {
+        tag : "music",
+        posts : postConstants
+    },
+    {
+        tag : "movies",
+        posts : postConstants
+    },
+    {
+        tag : "education",
+        posts : postConstants
+    }
+];
+export default exploreConstants;
