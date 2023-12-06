1986fe4ad56d07bbc20a439785d1f19217811a81
Azeem Shaik <azeemshaik025@gmail.com>
2023-12-06 12:04:53 +0530

    Create Authentication Partials

--- src/Components/Authentication/Authentication.jsx
+++ src/Components/Authentication/Authentication.jsx
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from 'react'
 import SideImage from '../../assets/AuthSideImage.jpg';
 import './Authentication.css';
 import AuthenticationUtilityForm from './AuthenticationUtilityForm';
+import axios from 'axios';
 const Authentication = () => {
     const [path, setPath] = useState({
         heading: 'Sign In',
         changeMessage: 'Registered? ',
         type: 'Login',
+    });
+    const [auth, setAuth] = useState({
+        email: '',
+        name: '',
+        password: '',
+        phNumber: ''
     });
     const [mobile, setMobile] = useState(false);
     const changePath = () => {
@@ -25,6 +32,31 @@ const Authentication = () => {
             })
         }
     }
+    const changeInput = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        setAuth((prevValue) => {
+            return { ...prevValue, [name]: value }
+        })
+    }
+    const Authenticate = async (e) => {
+        e.preventDefault();
+        const { name } = e.target;
+        if (name === 'Login') {
+            const backendData = {
+                email: auth.email,
+                password: auth.password
+            }
+            const response = await axios.post('#', { backendData });
+            const data = await response.data;
+            console.log(data);
+        }
+        else {
+            const response = await axios.post('#', { auth });
+            const data = await response.data;
+            console.log(data);
+        }
+    }
     useEffect(() => {
         const windowSize = window.innerWidth;
         if (windowSize < 992) {
@@ -34,7 +66,7 @@ const Authentication = () => {
     return (
         <div id='authentication-main'>
             <div id='authentication-dynamics'>
-                <AuthenticationUtilityForm heading={path.heading} changeMessage={path.changeMessage} type={path.type} changePath={changePath} />
+                <AuthenticationUtilityForm changeInput={changeInput} Authenticate={Authenticate} heading={path.heading} changeMessage={path.changeMessage} type={path.type} changePath={changePath} />
             </div>
             {!mobile && <img src={SideImage} alt="Auth" />}
         </div >
--- src/Components/Authentication/AuthenticationUtilityForm.jsx
+++ src/Components/Authentication/AuthenticationUtilityForm.jsx
@@ -4,11 +4,11 @@ const AuthenticationUtilityForm = (props) => {
     return (
         <div id='authentication-utility-main'>
             <h1>{props.heading}</h1>
-            <form onSubmit={null}>
-                <input type="email" required placeholder='Enter Your Email' />
-                <input type="email" required placeholder='Enter Your Email' />
-                <input type="email" required placeholder='Enter Your Email' />
-                <input type="email" required placeholder='Enter Your Email' />
+            <form name={props.type} onSubmit={props.Authenticate}>
+                {props.type === 'Register' && <input name='name' type="text" onChange={props.changeInput} required placeholder='Enter Your Name' />}
+                <input name='email' type="email" onChange={props.changeInput} required placeholder='Enter Your Email' />
+                <input name='password' type="password" onChange={props.changeInput} required placeholder='Enter Your Password' />
+                {props.type === 'Register' && <input name='phNumber' type="number" onChange={props.changeInput} required placeholder='Enter Your Number' />}
                 <button type='submit'>{props.type}</button>
             </form>
             <p>Already {props.changeMessage} <button onClick={props.changePath}>Change</button></p>
