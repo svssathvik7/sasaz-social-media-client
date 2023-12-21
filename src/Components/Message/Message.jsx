f00b0bd4d19808b645d51e2ba005d1436cc096d8
Azeem Shaik <azeemshaik025@gmail.com>
2023-12-21 22:05:46 +0530

    Create interface for messaging

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { BsEmojiSmile } from "react-icons/bs";
+import OnlineFriends from '../OnlineFriends/OnlineFriends';
+import './Message.css';
+import { userContextProvider } from '../Contexts/UserContext';
 
 const Message = () => {
+    const { user } = useContext(userContextProvider);
     return (
-        <div>Message</div>
+        <div id='message-page'>
+            <OnlineFriends />
+            <div id='messaging-block'>
+                <div id='profile-box'>
+                    {user && <img src={user.dp} alt="" />}
+                    <p>Sathvik Shaik</p>
+                </div>
+                <div id='messaging-box'>
+                    Here all the messages will appear
+                </div>
+                <div id="input-message-box">
+                    <BsEmojiSmile />
+                    <input type="text" placeholder='Enter Your Message' />
+                    <button>Send</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
