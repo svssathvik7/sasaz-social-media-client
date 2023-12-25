17e367d44e28a230af938de41cb8aa7af53c03a6
Azeem Shaik <108066377+Azeem-0@users.noreply.github.com>
2023-12-25 21:38:58 +0530

    Online Friends Dynamics

--- src/Components/OnlineFriends/OnlineFriends.jsx
+++ src/Components/OnlineFriends/OnlineFriends.jsx
@@ -1,68 +1,35 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState,useContext,useEffect } from 'react';
+import { userContextProvider } from '../Contexts/UserContext';
+import axios from 'axios';
 import "./OnlineFriends.css";
 export default function OnlineFriends() {
-  // dummy data
-  const [frnds, setFrnds] = useState([
-    {
-      id: 1,
-      username: "svs sathvik",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 2,
-      username: "azeem shaik",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 3,
-      username: "Friend 3",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 4,
-      username: "Friend 4",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 5,
-      username: "Friend 5",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 6,
-      username: "Friend 6",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 7,
-      username: "Friend 7",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 8,
-      username: "Friend 8",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 9,
-      username: "Friend 9",
-      displaypicture: require("../../assets/image.png")
-    },
-    {
-      id: 10,
-      username: "Friend 10",
-      displaypicture: require("../../assets/image.png")
-    },
-  ]);
+  const { user } = useContext(userContextProvider);
+  const [friends, setFriends] = useState([]);
+    useEffect(() => {
+      if (user && user.email) { 
+        axios.post('http://localhost:5001/api/user/getallfrnds', { email: user.email })
+          .then(response => {
+            if (response.data.status) {
+              
+              setFriends(response.data.friends);
+            } else {
+              console.log(response.data.message);
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    }, [user]);
   return (
     <div id='online-friends'>
       {/* <h2 id='online-friends-heading'>Online Friends</h2> */}
       <div id='online-friends-scroller'>
-        {frnds.map(frnd => (
+        {friends.map(frnd => (
           <div key={frnd.id} id='friend-div'>
-            <img alt={"Dp"} src={frnd.displaypicture} />
-            <h4>{frnd.username}</h4>
+            <img alt={"Dp"} src={frnd.dp} style={{ width: '50px', height: '50px' }} />
+            <h4>{frnd.name}</h4>
           </div>
         ))}
       </div>
